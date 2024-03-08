a201073f11b6b0baa1340c37ad355d073924b143
sem22-dev <thotjj22@gmail.com>
2024-03-08 06:54:47 +0530

    sparkline

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['assets.coingecko.com'],
+        domains: ['assets.coingecko.com', 'coingecko.com'],
       },
 };
 
