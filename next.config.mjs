bab10f620c50af886715c23d18d211af0d59cd96
sem22-dev <thotjj22@gmail.com>
2024-03-08 13:58:19 +0530

    remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['assets.coingecko.com', 'coingecko.com'],
+      images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'assets.coingecko.com',
+            pathname: '**',
+          },
+        ],
       },
 };
 
